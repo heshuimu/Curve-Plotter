7ec584ce30c026cd6cb12474ff69807fe24aa386
Tommy Li <h403l224@ku.edu>
2016-09-22 17:17:16 -0500

    Deleted Derived Data

--- DerivedData/Curve Plotter/Build/Intermediates/Curve Plotter.build/Debug/Curve Plotter.build/Objects-normal/x86_64/ShaderIF.d
+++
@@ -1,4 +0,0 @@
-dependencies: \
-  /Users/Yuki/Documents/Xcode/EECS\ 672/Curve\ Plotter/Curve\ Plotter/ShaderIF.c++ \
-  /Users/Yuki/Documents/Xcode/EECS\ 672/Curve\ Plotter/Curve\ Plotter/ShaderIF.h \
-  /usr/local/include/GLFW/glfw3.h
--- DerivedData/Curve Plotter/Build/Intermediates/Curve Plotter.build/Debug/Curve Plotter.build/Objects-normal/x86_64/project1.d
+++
@@ -1,7 +0,0 @@
-dependencies: \
-  /Users/Yuki/Documents/Xcode/EECS\ 672/Curve\ Plotter/Curve\ Plotter/project1.c++ \
-  /Users/Yuki/Documents/Xcode/EECS\ 672/Curve\ Plotter/Curve\ Plotter/GLFWController.h \
-  /usr/local/include/GLFW/glfw3.h \
-  /Users/Yuki/Documents/Xcode/EECS\ 672/Curve\ Plotter/Curve\ Plotter/Controller.h \
-  /Users/Yuki/Documents/Xcode/EECS\ 672/Curve\ Plotter/Curve\ Plotter/ModelView.h \
-  /Users/Yuki/Documents/Xcode/EECS\ 672/Curve\ Plotter/Curve\ Plotter/ShaderIF.h
